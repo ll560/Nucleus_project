42a3f7111fe37aa975dd2e5b1dfdb05130d821bd
ll560 <llavia6@gmail.com>
2021-11-04 17:29:33 -0400

    chart complete, working mvp

--- public/script.js
+++ public/script.js
@@ -45,63 +45,88 @@ function closeSidebar() {
 
 
 /*-----------chart data---------------------*/
-let chartData = [];
+document.addEventListener('DOMContentLoaded', event => {
+  function createAndMaintainChart(parsedData) {
+    console.log('top of createAndMaintain', parsedData);
+    let surveyData = [];
 
-let fetchSurveyData = async () => {
-  let data = await fetch('http://localhost:3000/survey/show.json') 
-  let parsedData = await data.json()
-  console.log(parsedData)
-  return chartData.push(parsedData) /* let not needed b/c overriding data */
-  
-};
+    const populateChart = () => {
+      let chem = 0;
+      let bio = 0;
+      let cars = 0;
+      let psychology = 0;
+      let notKnown = 0;
 
-fetchSurveyData()
+      parsedData.forEach(survey => {
+        if (survey.favorite_section === 'bio_boichem') {
+          return (bio += 1);
+        } else if (survey.favorite_section === 'chem_phys') {
+          return (chem += 1);
+        } else if (
+          survey.favorite_section === 'critical_analysis_and_reasoning'
+        ) {
+          return (cars += 1);
+        } else if (survey.favorite_section === 'psychology') {
+          return (psychology += 1);
+        } else {
+          return (notKnown += 1);
+        }
+      });
 
-if (chartData !== []) {
-  
-  console.log(chartData);
-  
-  
-}
-//.forEach(survey => survey)
+      return (surveyData = [chem, bio, cars, psychology, notKnown]);
+    };
 
-//check if it exist
-if (document.getElementById('myChart')){
-let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
-  type: 'bar', /* changes the chart type */
-  data: {
-      labels: ['Months', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+    populateChart();
+    console.log({ surveyData });
+
+    //check if it exist
+    if (document.getElementById('myChart')) {
+      let ctx = document.getElementById('myChart').getContext('2d');
+      let myChart = new Chart(ctx, {
+        type: 'bar' /* changes the chart type */,
+        data: {
+          labels: ['Chem', 'Bio', 'Cars', 'Psychology', 'unknown'],
+          datasets: [
+            {
+              label: 'favorite sections',
+              data: surveyData,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
           ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          y: {
-              beginAtZero: true
-          }
-      }
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    }
   }
+
+  // init fetches
+  const init = async () => {
+    let data = await fetch('http://localhost:3000/survey/show.json');
+    let parsedData = await data.json();
+    console.log(parsedData.data[0]);
+    createAndMaintainChart(parsedData.data);
+  };
+  init();
 });
-}; 
-
-
